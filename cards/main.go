88f24b39ca4eb559dbc3ddac3144f6c965b301d6
Sirwan <sir1afifi@gmail.com>
2021-08-22 17:01:41 +0430

    variables

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// var card string = "Ace of Spades"
+	// := rely on Go compiler to infer the variable's type
+	// we only use := when we are defining a new variable
+	card := "Ace of Spades"
+	
+	fmt.Println(card)
+}
