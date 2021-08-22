5f540f8bd5b4db1b5e9c95245faf41bda974c63c
Sirwan <sir1afifi@gmail.com>
2021-08-22 17:09:56 +0430

    function

--- cards/main.go
+++ cards/main.go
@@ -4,10 +4,11 @@ import "fmt"
 
 
 func main() {
-	// var card string = "Ace of Spades"
-	// := rely on Go compiler to infer the variable's type
-	// we only use := when we are defining a new variable
-	card := "Ace of Spades"
+	card := newCard()
 	
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
