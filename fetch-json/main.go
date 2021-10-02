da61e28b9366da98cba99f1155c0ba90703ab436
Sirwan <sir1afifi@gmail.com>
2021-10-02 19:48:48 +0330

    Add parser

---
+++ fetch-json/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type user struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+}
+
+type response struct {
+  PageCount int `json:"pageCount"`
+  TotalCounts int `json:"totalCounts"`
+  Posts []user `json:"posts"`
+}
+
+func main() {
+	url := flag.String("url", "localhost:8888/api/users", "URL To Resource")
+	flag.Parse()
+
+	client := http.Client {
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, *url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "sirwan")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := response{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, u := range result.Posts {
+		fmt.Printf("User ID: %s - Full Name: %s\n", u.Id, u.Name)
+	}
+}
